e9226b748a24c9c5a4ed332bd9535863094ba1a3
Vajniy <sanya92t@gmail.com>
2019-06-13 23:54:34 +0300

    push

---
+++ catalog/urls.py
@@ -0,0 +1,26 @@
+from django.urls import path
+from . import views
+from django.conf.urls import url
+
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('books/', views.BookListView.as_view(), name='books'),
+    path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
+    path('authors/', views.AuthorListView.as_view(), name='authors'),
+    path('author/<int:pk>', views.AuthorDetailView.as_view(), name='author-detail'),
+    url(r'^mybooks/$', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
+    path('borrowed/', views.SeeAllBorrowers.as_view(), name='all-borrowers')
+]
+
+#URL-адреса для страницы обновления книг
+urlpatterns += [   
+    url(r'^book/(?P<pk>[-\w]+)/renew/$', views.renew_book_librarian, name='renew-book-librarian'),
+]
+
+#Create, update, delete Для модели Автора
+urlpatterns += [  
+    url(r'^author/create/$', views.AuthorCreate.as_view(), name='author_create'),
+    url(r'^author/(?P<pk>\d+)/update/$', views.AuthorUpdate.as_view(), name='author_update'),
+    url(r'^author/(?P<pk>\d+)/delete/$', views.AuthorDelete.as_view(), name='author_delete'),
+]
