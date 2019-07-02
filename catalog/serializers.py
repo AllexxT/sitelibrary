d8b20c4efca6c1acbedddfc0efe69d1a00ea4a31
Vajniy <sanya92t@gmail.com>
2019-07-02 12:20:23 +0300

    fix API one to many field in first_name, last_name of authors

--- catalog/serializers.py
+++ catalog/serializers.py
@@ -3,9 +3,11 @@ from rest_framework import serializers
 
 
 class BookSerializer(serializers.HyperlinkedModelSerializer):
+    first_name = serializers.CharField(source='author.first_name', read_only=True)
+    last_name = serializers.CharField(source='author.last_name', read_only=True)
     class Meta:
         model = Book
-        fields = ('author', 'title', 'summary', 'isbn')
+        fields = ( 'author', 'first_name', 'last_name', 'title', 'summary', 'isbn',)
 
 class AuthorSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
